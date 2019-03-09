bbab81cb466120429137deb15c22cfed59a9c802
None <None>
2019-03-09 02:46:15 -0800

    Add FW and params support to Fake
    Signed-off-by: None <None>

--- src/main/java/org/moonlightcontroller/samples/Fake.java
+++ src/main/java/org/moonlightcontroller/samples/Fake.java
@@ -15,7 +15,15 @@ import java.net.*;
 import java.util.*;
 import java.util.LinkedList;
 import java.util.logging.Logger;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.moonlightcontroller.samples.actions.Action;
+import org.moonlightcontroller.samples.actions.ActionAlert;
+import org.moonlightcontroller.samples.actions.ActionDrop;
+import org.moonlightcontroller.samples.actions.ActionLog;
+import org.moonlightcontroller.samples.actions.ActionOutput;
 import org.openboxprotocol.exceptions.InstanceNotAvailableException;
 import org.moonlightcontroller.bal.BoxApplication;
 import org.moonlightcontroller.blocks.FromDevice;
@@ -28,6 +36,7 @@ import org.moonlightcontroller.blocks.HeaderClassifier.HeaderClassifierRule;
 import org.moonlightcontroller.blocks.ToDevice;
 import org.moonlightcontroller.blocks.ToDump;
 import org.moonlightcontroller.blocks.Alert;
+import org.moonlightcontroller.blocks.Log;
 import org.moonlightcontroller.mtd.BlockProtector;
 import org.moonlightcontroller.mtd.ApplicationType;
 import org.moonlightcontroller.processing.Connector;
@@ -57,6 +66,10 @@ import org.openboxprotocol.protocol.OpenBoxHeaderMatch;
 import org.openboxprotocol.protocol.Priority;
 import org.openboxprotocol.protocol.Statement;
 import org.openboxprotocol.types.TransportPort;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
 
 import com.google.common.collect.ImmutableList;
 
@@ -64,18 +77,57 @@ public class Fake extends BoxApplication {
 
 	private final static Logger LOG = Logger.getLogger(Fake.class.getName());
 
+	public static final String PROPERTIES_PATH = "Fake.properties";
+
 	public static final String PROP_PATTERNS_FILE = "patterns_file";
-	public static final long PROP_APPLICATION_TYPE = 0;
+	public static final String PROP_APPLICATION_TYPE = "app_type";
+	public static final String PROP_SEGMENT = "segment";
+	public static final String PROP_IN_IFC = "in_ifc";
+	public static final String PROP_RULE_FILE = "rule_file";
+	public static final String PROP_NETMAP_EN = "netmap_enabled";
+
+	public static final String DEFAULT_SEGMENT = "220";
+	public static final String DEFAULT_IN_IFC = "eth1";
+	public static final String DEFAULT_RULE_FILE = "firewall_rules.txt";
+	public static final String DEFAULT_NETMAP_EN = "false";
+	public static final String DEFAULT_PATTERNS_FILE = "patterns_file";
+	public static final String DEFAULT_APPLICATION_TYPE = "0";
+
+	private static final Properties DEFAULT_PROPS = new Properties();
+	static {
+		DEFAULT_PROPS.setProperty(PROP_SEGMENT, DEFAULT_SEGMENT);
+		DEFAULT_PROPS.setProperty(PROP_APPLICATION_TYPE, DEFAULT_APPLICATION_TYPE);
+		DEFAULT_PROPS.setProperty(PROP_IN_IFC, DEFAULT_IN_IFC);
+		DEFAULT_PROPS.setProperty(PROP_PATTERNS_FILE, DEFAULT_PATTERNS_FILE);
+		DEFAULT_PROPS.setProperty(PROP_RULE_FILE, DEFAULT_RULE_FILE);
+		DEFAULT_PROPS.setProperty(PROP_NETMAP_EN, DEFAULT_NETMAP_EN);
+	}
+
+	private Properties props;
 
 	public Fake() {
 		super("The most Fake app in the world", Priority.HIGH);
+
+		props = new Properties(DEFAULT_PROPS);
+		File f = new File(PROPERTIES_PATH);
+		try {
+			props.load(new FileReader (f));
+		} catch (IOException e) {
+			LOG.severe("Cannot load properties file from path: " + f.getAbsolutePath());
+			LOG.severe("Using default properties.");
+		}
+		LOG.info(String.format("Fake is running on Segment %s", props.getProperty(PROP_SEGMENT)));
+		LOG.info(String.format("[->] Interface for input: %s", props.getProperty(PROP_IN_IFC)));
+		LOG.info(String.format("[->] Patterns file path: %s", props.getProperty(PROP_PATTERNS_FILE)));
+		LOG.info(String.format("[>|] Rule files path: %s", props.getProperty(PROP_RULE_FILE)));
+		LOG.info(String.format("[>|] Netmap enabled: %s", props.getProperty(PROP_NETMAP_EN)));
+
 		List<IStatement> statements = createStatements();
-		System.out.println("KOKO" + statements.get(0).getLocation().getId());
 		this.setStatements(statements);
 		this.setInstanceUpListener(new InstanceUpHandler());
 		this.setAlertListener(new FakeAlertListener());
 		this.setCastleListener(new FakeCastleListener());
-		this.setType(new ApplicationType(PROP_APPLICATION_TYPE));
+		this.setType(new ApplicationType(Long.parseLong(props.getProperty(PROP_APPLICATION_TYPE))));
 	}
 
 	private List<String> readPatterns(String path) {
@@ -115,6 +167,51 @@ public class Fake extends BoxApplication {
 		}
 		return result;
 	}
+
+	private List<String> readPatternsSimple(String path) {
+		List<String> result = new ArrayList<>();
+		int ret = 0;
+		File f = new File(path);
+		BufferedReader reader = null;
+		System.out.println("Reading patterns simple...");
+		try {
+			reader = new BufferedReader(new FileReader(f));
+			int index = 0;
+			while (true) {
+				String line;
+				line = reader.readLine();
+				if (line == null) {
+					break;
+				} else{
+					index++;
+					result.add(line);
+				}
+			}
+			LOG.info("Read " + result.size() + " patterns from file " + path);
+		} catch (IOException e) {
+			LOG.severe("Error (" + e.getClass().getName() + ") while reading patterns from file (MESSAGE=" + e.getMessage() + ")");
+		} finally {
+			if (reader != null) {
+				try { reader.close(); } catch (Exception e) { }
+			}
+		}
+		return result;
+	}
+
+	private String readInterfaceHint(String path) {
+		byte[] encoded = null;
+		try {
+			System.out.println("Reading interface hint...");
+			encoded = Files.readAllBytes(Paths.get(path));
+		} catch (IOException e) {
+			LOG.severe("Error (" + e.getClass().getName() + ") while reading patterns from file (MESSAGE=" + e.getMessage() + ")");
+			return "";
+		}
+		String iface = new String(encoded, Charset.defaultCharset());
+		System.out.println("Interface hint is " + iface);
+		return iface;
+	}
+	
 
 	@Override
 	public void handleAppStart(IApplicationTopology top, IHandleClient handles) {
@@ -166,7 +263,7 @@ public class Fake extends BoxApplication {
 			if (message instanceof ReadResponse){
 				ReadResponse rr = (ReadResponse)message;
 				LOG.info("got a read response:" + rr.getBlockId() + "::" + rr.getReadHandle() + "::" + rr.getResult());
-			}			
+			}
 		}
 
 		@Override
@@ -176,102 +273,193 @@ public class Fake extends BoxApplication {
 	}
 
 	private List<IStatement> createStatements(){
-		String iface = "lo";
+		String name = props.getProperty(PROP_IN_IFC);
+		boolean netmap_en = Boolean.parseBoolean(props.getProperty(PROP_NETMAP_EN));
+		String iface = name;
 		try {
-		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        for (NetworkInterface netint : Collections.list(nets)) {
-			if (netint.getName().contains("eth0")) {
-				iface = netint.getName();
+			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+			System.out.println(nets);
+			for (NetworkInterface netint : Collections.list(nets)) {
+				System.out.println("Checking: " + netint.getName() + " vs " + name);
+				System.out.println(netint.getName() + " == " + name + " is " + netint.getName().contains(name.trim()));
+				if (netint.getName().contains(name.trim())) {
+					iface = netint.getName();
+					break;
+				}
 			}
-		}
 		} catch (SocketException exp) {
 			System.out.print("socket exception while enumerating interfaces");
 		}
-		HeaderMatch h1 = new OpenBoxHeaderMatch.Builder().setExact(HeaderField.TCP_DST, new TransportPort(22)).build();
-		HeaderMatch h2 = new OpenBoxHeaderMatch.Builder().build();
-		ArrayList<HeaderClassifierRule> rules = new ArrayList<HeaderClassifierRule>(Arrays.asList(
-				new HeaderClassifierRule.Builder().setHeaderMatch(h1).setPriority(Priority.HIGH).setOrder(0).build(),
-				new HeaderClassifierRule.Builder().setHeaderMatch(h2).setPriority(Priority.HIGH).setOrder(1).build()));
-		FromDevice from = new FromDevice("FromDevice_FakeApp", iface, true, true);
-		//FromDump from = new FromDump("FromDump_FakeApp", "/home/mininet/openbox/MoonlightFake/dummy_dump.pcap", false, false);
-		ToDump to1 = new ToDump("ToDump1_FakeApp", "/home/mininet/hello_ssh.pcap");
-		// ToDump to2 = new ToDump("ToDump2_FakeApp", "/home/mininet/hello_rest.pcap");
-		FromHost fh = new FromHost("FromHost_FakeApp", "virt");
-		ToHost th = new ToHost("ToHost_FakeApp", "virt");
+
+		List<IConnector> connectors = new ArrayList<>();
+		List<IProcessingBlock> blocks = new ArrayList<>();
+		FromDevice from;
+
+		if (netmap_en) {
+			from = new FromDevice("FromDevice_FakeApp", iface, false, true, true);
+		} else {
+			from = new FromDevice("FromDevice_FakeApp", iface, true, true, false);
+		}
+		Map<String, IProcessingBlock> toDeviceBlocks = new HashMap<>();
+		Map<String, Alert> alertBlocks = new HashMap<>();
+		//FromHost fh = new FromHost("FromHost_FakeApp", "virt");
+		//ToHost th = new ToHost("ToHost_FakeApp", "virt");
 		ToDump discard = new ToDump("ToDump3_FakeApp", "/home/mininet/hello_malicious.pcap");
-		List<String> dpiPatterns = readPatterns(PROP_PATTERNS_FILE);
-		HeaderClassifier classify = new HeaderClassifier("Classify_FakeApp", rules, Priority.HIGH, false);
+		Alert alert = new Alert("Alert_FakeApp", "DPI malicious packet alert", 1, true, 1000);
+		//ToDevice to = new ToDevice("ToDevice_FakeApp_" + props.getProperty(PROP_IN_IFC), props.getProperty(PROP_IN_IFC), false);
+
+		blocks.add(from);
+		//blocks.add(alert);
+		//blocks.add(fh);
+		//blocks.add(to);
+		//blocks.add(th);
+		blocks.add(discard);
+		// ===================== Snort ==========================
+		List<String> dpiPatterns = readPatternsSimple(props.getProperty(PROP_PATTERNS_FILE));
 		List<StringMatcher> dpis = new LinkedList<>();
+		alertBlocks.put(alert.getMessage(), alert);
+		//toDeviceBlocks.put(props.getProperty(PROP_IN_IFC), to);
 		dpis.add(new StringMatcher("StringMatcher_WM_FakeApp", dpiPatterns, "wumanber")); // chhose from: wumanber, ahocorasick, compressedahocorasick
 		dpis.add(new StringMatcher("StringMatcher_AC_FakeApp", dpiPatterns, "ahocorasick"));
 		dpis.add(new StringMatcher("StringMatcher_CAC_FakeApp", dpiPatterns, "compressedahocorasick"));
-		Alert alert = new Alert("Alert_Fake", "Alert from Fake", 1, true, 1000);
-		//Discard discard = new Discard("Discard_Snort");
+		// ===================== Firewall =======================
+		List<HeaderClassifierRule> headerRules = new ArrayList<>();
+		List<Rule> rules;
+
+		try {
+			RuleParser rp = new RuleParser(props.getProperty(PROP_RULE_FILE));
+			System.out.println("After rp!");
+			rules = rp.read();
+		} catch (Exception e) {
+			LOG.severe("Failed to parse rule file: " + e.getMessage() + "\n" + e.getStackTrace());
+			return ImmutableList.of();
+		}
+
+		HeaderClassifier classify = new HeaderClassifier("HeaderClassifier_FakeApp", headerRules, Priority.HIGH, false);
+		blocks.add(classify);
+
+		connectors.add(new Connector.Builder()
+				.setSourceBlock(from)
+				.setSourceOutputPort(0)
+				.setDestBlock(classify).build()
+		);
+		// connectors.add(new Connector.Builder()
+		// 		.setSourceBlock(alert)
+		// 		.setSourceOutputPort(0)
+		// 		.setDestBlock(discard).build()
+		// );
+
+		// ========================= Graph ========================
 
 		IStatement.Builder stb = new Statement.Builder();
 		stb.setLocation(TopologyManager.getInstance().resolve(220));
 
 		for (StringMatcher dpi: dpis) {
-			List<IConnector> connectors = new ArrayList<>();
-			List<IProcessingBlock> blocks = new ArrayList<>();
+			toDeviceBlocks.clear();
+			alertBlocks.clear();
+			headerRules.clear();
+			List<IConnector> snort_connectors = new ArrayList<>();
+			List<IProcessingBlock> snort_blocks = new ArrayList<>();
 
 			BlockProtector prot = BlockProtector.getInstance();
-			IProcessingGraph prot_hdr_clas = prot.getProtectedSubGraph(dpi, 2, 10, 1.2);
-			List<IProcessingBlock> prot_blocks = prot_hdr_clas.getBlocks();
-			IProcessingBlock prot_out = prot_blocks.get(prot_blocks.size() - 1);
-			blocks.addAll(prot_blocks);
-			// blocks.addAll(ImmutableList.of(from, classify, alert, to1, discard, to2));
-			blocks.addAll(ImmutableList.of(from, classify, alert, to1, discard, fh, th));
-			connectors.addAll(prot_hdr_clas.getConnectors());
-			connectors.addAll(ImmutableList.of(
-					new Connector.Builder()
-						.setSourceBlock(from)
-						.setSourceOutputPort(0)
-						.setDestBlock(classify)
-						.build(),
-					new Connector.Builder()
-						.setSourceBlock(classify)
-						.setSourceOutputPort(0)
-						.setDestBlock(to1)
-						.build(),
-					new Connector.Builder()
-						.setSourceBlock(classify)
-						.setSourceOutputPort(1)
-						.setDestBlock(prot_hdr_clas.getRoot())
-						.build(),
-					new Connector.Builder()
-						.setSourceBlock(prot_out)
-						.setSourceOutputPort(1)
-						.setDestBlock(alert)
-						.build(),
-					// new Connector.Builder()
-					// 	.setSourceBlock(prot_out)
-					// 	.setSourceOutputPort(0)
-					// 	.setDestBlock(to2)
-					// 	.build(),
-					new Connector.Builder()
-						.setSourceBlock(prot_out)
-						.setSourceOutputPort(0)
-						.setDestBlock(th)
-						.build(),
-					new Connector.Builder()
-						.setSourceBlock(alert)
-						.setSourceOutputPort(0)
-						.setDestBlock(discard)
-						.build(),
-					new Connector.Builder()
-						.setSourceBlock(fh)
-						.setSourceOutputPort(0)
-						.setDestBlock(th)
-						.build()
-					));
+			IProcessingGraph prot_hdr_clas;
+			List<IProcessingBlock> prot_blocks;
+			IProcessingBlock prot_out;
 
+			// Add header FW
+			int i = 0;
+			for (Rule r : rules) {
+				headerRules.add(new HeaderClassifierRule.Builder()
+					.setHeaderMatch(r.getHeaderMatch())
+					.setPriority(r.getPriority())
+					.setOrder(i)
+					.build());
+				
+				IProcessingBlock last = classify;
+				int lastOutPort = i;
+				int j = 0;
+				boolean stop = false;
+				boolean exists;
+				for (Action action : r.getActions()) {
+					IProcessingBlock destBlock;
+					IProcessingBlock blockToAdd;
+					exists = false;
+					String suffix = String.format("_Firewall_Rule_%d_UID_%d", i, j);
+					String msg;
+					if (action instanceof ActionAlert) {
+						msg = ((ActionAlert)action).getMessage();
+						if (alertBlocks.containsKey(((ActionAlert)action).getMessage())) {
+							destBlock = alertBlocks.get(msg);
+							blockToAdd = destBlock;
+							exists = true;
+						} else {
+							Alert newBlock = new Alert("Alert_FakeApp" + suffix, msg);
+							alertBlocks.put(msg, newBlock);
+							destBlock = newBlock;
+							blockToAdd = destBlock;
+						}
+					} else if (action instanceof ActionOutput) {
+						String out_iface = ((ActionOutput)action).getInterface();
+						if (toDeviceBlocks.containsKey(out_iface)) {
+							destBlock = toDeviceBlocks.get(out_iface);
+							blockToAdd = destBlock;
+							exists = true;
+						} else {
+							ToDevice newBlock = new ToDevice("ToDevice_FakeApp_" + out_iface, out_iface, netmap_en);
+							prot_hdr_clas = prot.getProtectedSubGraph(dpi, 2, 1000, 3);
+							prot_blocks = prot_hdr_clas.getBlocks();
+							prot_out = prot_blocks.get(prot_blocks.size() - 1);
+							snort_connectors.add(new Connector.Builder()
+									.setSourceBlock(prot_out)
+									.setSourceOutputPort(0)
+									.setDestBlock(newBlock).build()
+							);
+							snort_connectors.add(new Connector.Builder()
+									.setSourceBlock(prot_out)
+									.setSourceOutputPort(1)
+									.setDestBlock(newBlock).build()
+							);
+							snort_blocks.addAll(prot_blocks);
+							snort_connectors.addAll(prot_hdr_clas.getConnectors());
+							destBlock = prot_hdr_clas.getRoot();
+							blockToAdd = newBlock;
+							toDeviceBlocks.put(out_iface, destBlock);
+						}
+					} else if (action instanceof ActionDrop) {
+						destBlock = discard;
+						blockToAdd = destBlock;
+						exists = true;
+						stop = true;
+					} else if (action instanceof ActionLog) {
+						msg = ((ActionLog)action).getMessage();
+						destBlock = new Log("Log" + suffix, msg);
+						blockToAdd = destBlock;
+					} else {
+						LOG.severe("Unknown action: " + action.getType());
+						continue;
+					}
+					if (!exists)
+						snort_blocks.add(blockToAdd);
+
+					snort_connectors.add(new Connector.Builder().setSourceBlock(last).setSourceOutputPort(lastOutPort).setDestBlock(destBlock).build());
+					last = destBlock;
+					lastOutPort = 0;
+					j++;
+					if (stop)
+						break;
+				}
+				i++;
+			}
+			// end
 			IProcessingGraph graph = new ProcessingGraph.Builder()
-				.setBlocks(blocks)
-				.setConnectors(connectors)
+				.setBlocks(Stream.concat(blocks.stream(), snort_blocks.stream())
+					.collect(Collectors.toList()))
+				.setConnectors(Stream.concat(connectors.stream(), snort_connectors.stream())
+					.collect(Collectors.toList()))
 				.setRoot(from)
 				.build();
 
+			System.out.println(graph.getBlocks());
 			stb.setProcessingGraph(graph);
 		}
 		
